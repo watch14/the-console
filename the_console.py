2a99a414be0ee7bd4396abc00a66517b5aed3054
watch14 <watchman1414@gmail.com>
2023-12-04 09:44:09 +0100

    documented

--- the_console.py
+++ the_console.py
@@ -4,11 +4,13 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
+        """" Quit hbnb """
         return True
     
     do_EOF = do_quit
 
     def do_help(self, arg):
+        """" help hbnb """
         cmd.Cmd.do_help(self, arg)
 
     def emptyline(self):
