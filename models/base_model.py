41c556400161f81f878500aa425fa1dcb2429b71
watch14 <watchman1414@gmail.com>
2023-12-04 16:03:42 +0100

    task3 Done

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,17 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Public instance attributes"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """return string info"""
-        return ('[' + type(self).__name__ + '] ('
-                + str(self.id) + ') ' + str(self.__dict__))
+        name = self.__class__.__name__
+        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
 
     def save(self):
         """ save / update """
-        self.updated_at = str(datetime.now())
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """return a dictionary of the instance"""
@@ -28,3 +28,17 @@ class BaseModel():
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+    
+    
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
