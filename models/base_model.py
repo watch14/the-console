641e8fea3bef4e4759e77130ead688502ee7ff7e
watch14 <watchman1414@gmail.com>
2023-12-05 13:45:19 +0100

    5/12 2pm

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 """BaseModel"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel():
     """BaseModel Class"""
@@ -22,6 +22,7 @@ class BaseModel():
                     self.updated_at = datetime.strptime(value, time_form)
                 if key == "id":
                     self.id = value
+        models.storage.new(self)
 
     def __str__(self):
         """return string info"""
@@ -31,6 +32,7 @@ class BaseModel():
     def save(self):
         """ save / update """
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """return a dictionary of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,3 +9,35 @@ class FileStorage ():
         """attributes"""
         self.__file_path = "file.json"
         self.__objects = {}
+
+    def all(self):
+        """returns __objects"""
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """set obj"""
+        k_o = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[k_o] = obj
+
+    def save(self):
+        """Sereialize JSON"""
+        obj_dic = {}
+        for key in FileStorage.__objects.keys():
+            obj = FileStorage.__objects[key]
+            obj_dic[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dic, f)
+
+    def reload(self):
+        """Deserialize JSON"""
+        try:
+            with open(FileStorage.__file_path, "r") as f:
+                obj_dic = json.load(f)
+
+            for value in obj_dic.values():
+                n_cls = value["__class__"]
+                del value["__class__"]
+                self.new(eval(n_cls)(**value))
+        except FileNotFoundError:
+            return
